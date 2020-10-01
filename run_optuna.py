a1b8d16bb6395162ce74abad1dd7779185a194e3
takuya-tokumoto <takuya.0810@hotmail.co.jp>
2020-10-01 21:59:29 +0900

    20201001

--- run_optuna.py
+++ run_optuna.py
@@ -8,11 +8,11 @@ import numpy as np
 
 from utils import load_datasets, load_target
 from logs.logger import log_best
-from models.lgbm import train_and_predict
+from models.lgbm_optuna import train_and_predict
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--config', default='./configs/default.json')
+parser.add_argument('--config', default='./configs/default_optuna.json')
 options = parser.parse_args()
 config = json.load(open(options.config))
 
@@ -27,46 +27,14 @@ logging.debug(feats)
 
 target_name = config['target_name']
 
-X_train, X_test = load_datasets(feats, target_name)
-y_train = load_target(target_name)
+X_train_all, X_test = load_datasets(feats, target_name)
+y_train_all = load_target(target_name)
 logging.debug(X_train_all.shape)
 
 y_preds = []
 models = []
 
 lgbm_params = config['lgbm_params']
-
-
-def objective(trial):
-    tr_train_x, tr_test_x, train_y, test_y = train_test_split(data, target, test_size=0.25)
-    dtrain = lgb.Dataset(train_x, label=train_y)
- 
-    param = {
-        'objective': 'binary',
-        'metric': 'binary_logloss',
-        'lambda_l1': trial.suggest_loguniform('lambda_l1', 1e-8, 10.0),
-        'lambda_l2': trial.suggest_loguniform('lambda_l2', 1e-8, 10.0),
-        'num_leaves': trial.suggest_int('num_leaves', 2, 256),
-        'feature_fraction': trial.suggest_uniform('feature_fraction', 0.4, 1.0),
-        'bagging_fraction': trial.suggest_uniform('bagging_fraction', 0.4, 1.0),
-        'bagging_freq': trial.suggest_int('bagging_freq', 1, 7),
-        'min_child_samples': trial.suggest_int('min_child_samples', 5, 100),
-    }
- 
-    gbm = lgb.train(param, dtrain)
-    preds = gbm.predict(test_x)
-    pred_labels = np.rint(preds)
-    accuracy = sklearn.metrics.accuracy_score(test_y, pred_labels)
-    return accuracy
- 
-study = optuna.create_study(direction='maximize')
-study.optimize(objective, n_trials=100)
- 
-print('Number of finished trials:', len(study.trials))
-print('Best trial:', study.best_trial.params)
-
-
-
 
 kf = KFold(n_splits=10, random_state=0)
 for train_index, valid_index in kf.split(X_train_all):
@@ -104,6 +72,10 @@ ID_name = config['ID_name']
 sub = pd.DataFrame(pd.read_csv('./data/input/test_data.csv')[ID_name])
 
 y_sub = sum(y_preds) / len(y_preds)
+
+print(y_sub)
+
+print(y_sub.shape)
 
 if y_sub.shape[1] > 1:
     y_sub = np.argmax(y_sub, axis=1)
